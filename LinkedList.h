a64e8fe5c001f824e1729f1ce0c1557b897a8833
Moey27 <62626189+Moey27@users.noreply.github.com>
2022-05-09 15:46:31 +1000

    Update LinkedlIst.h
    Implementing header file

--- LinkedList.h
+++ LinkedList.h
@@ -1,4 +1,3 @@
-
 #ifndef ASSIGN2_LINKEDLIST_H
 #define ASSIGN2_LINKEDLIST_H
 
@@ -10,8 +9,19 @@ public:
    LinkedList();
    ~LinkedList();
 
+   int size();
+   Node* get(int index);
+   void addBack(Tile* data);
+   void removeFront();
+   void removeBack();
+   void remove(int index);
+   void clearLinkedList();
+
+
+
 private:
    Node* head;
 };
 
 #endif // ASSIGN2_LINKEDLIST_H
+
